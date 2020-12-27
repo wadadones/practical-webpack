5c09a4eb72796527b2edf6896fcf0e26760cbef6
wadadones <jones3429@outlook.jp>
2020-12-27 15:45:15 +0900

    add babel in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
+// OS依存にならないようにpathモジュールを読み込んでおく
 const path = require('path')
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/app.js',
-  output: {
+  entry: './src/js/app.js', // どのファイルを読み始めるかを定義。ここにrequireが書いてあればそこからさらに読み込みを行う
+  output: { // 生成されたjsのファイル名と出力場所が定義されている
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // ローダーの処理対象を定義
+        include: path.resolve(__dirname, 'src/js'), // ローダーの処理対象となるディレクトリを定義
+        use: [
+          {
+            loader: 'babel-loader', // 利用するローダー
+            options: {
+              presets: [['@babel/preset-env', {modules: false}]]
+            }
+          }
+        ]
+      }
+    ]
   }
 }
