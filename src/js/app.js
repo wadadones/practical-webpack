20476ce367f562a33b27d5c55f921444fcdd954d
wadadones <jones3429@outlook.jp>
2020-12-27 15:32:07 +0900

    add js

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+import $ from 'jquery'; // ./node_modules/jquery/dist/jquery.js が読み込まれているわね！
+import add from './modules/add'; // export const add = (n1, n2) => n1 + n2;
+import tax from './modules/tax';
+
+const item1Price = 400;
+const item2Price = 600;
+const totalPrice = add(item1Price, item2Price);
+
+const salesTax = 1.08;
+const priceIncludeTax = tax(totalPrice, salesTax);
+
+$('.body').text(priceIncludeTax);
